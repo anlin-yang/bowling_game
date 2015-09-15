3042f54f22a2559d13abe3d78112de5efd28c966
anlin-yang <anlin0414@outlook.com>
2015-09-15 16:43:06 +0800

    add function addFrame() and pass the test.

--- main/match.js
+++ main/match.js
@@ -1,7 +1,16 @@
+var Frame = require('./frame.js');
+
 function Match() {
   this.frames = [];
   this.score;
   this.extraChances = [];
+}
+
+Match.prototype.addFrame = function(frameId, frameStr) {
+  var frame = new Frame(frameId, frameStr);
+  frame.addChance();
+  frame.setChanceScore();
+  this.frames.push(frame);
 }
 
 module.exports = Match;
