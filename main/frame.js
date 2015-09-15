80e04d1b72347f0bfbc02859e446cc85135a125f
anlin-yang <anlin0414@outlook.com>
2015-09-15 15:48:00 +0800

    add function addChance() , analysis valueStr to add attribute chances. from main/frame.js

--- main/frame.js
+++ main/frame.js
@@ -1,3 +1,5 @@
+var Chance = require('./chance.js');
+
 function Frame(frameId, valueStr) {
   this.frameId = frameId;
   this.valueStr = valueStr;
@@ -5,5 +7,13 @@ function Frame(frameId, valueStr) {
   this.chances = [];
   this.extraScore = 0;
 }
+
+Frame.prototype.addChance = function() {
+  var chances = this.valueStr.split('');
+  var that = this;
+  chances.forEach(function(val, index) {
+    that.chances.push(new Chance(index + 1, val));
+  });
+};
 
 module.exports = Frame;
