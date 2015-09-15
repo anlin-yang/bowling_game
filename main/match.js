d328606cb8ec96b5ad7708e8f8b1a810a035420c
anlin-yang <anlin0414@outlook.com>
2015-09-15 17:08:46 +0800

    add scaner() function to scan match string and pass the test.

--- main/match.js
+++ main/match.js
@@ -1,4 +1,5 @@
 var Frame = require('./frame.js');
+var Chance = require('./chance.js');
 
 function Match() {
   this.frames = [];
@@ -13,4 +14,17 @@ Match.prototype.addFrame = function(frameId, frameStr) {
   this.frames.push(frame);
 }
 
+Match.prototype.scaner = function(matchScoreStr) {
+  var framesAndChances = matchScoreStr.split('||');
+  var allFrames = framesAndChances[0].split('|');
+  var extraChances = framesAndChances[1].split('');
+  var that = this;
+
+  allFrames.forEach(function(val, index) {
+    that.addFrame(index + 1, val);
+  });
+  extraChances.forEach(function(val, index) {
+    that.extraChances.push(new Chance(index + 1, val));
+  });
+}
 module.exports = Match;
