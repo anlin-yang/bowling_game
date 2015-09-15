bdebdd48d9384a51a5ab2121a0ec71d9de997751
anlin-yang <anlin0414@outlook.com>
2015-09-15 16:37:03 +0800

    add test function addFrame to make sure correct add frame of object.

--- spec/main/match-spec.js
+++ spec/main/match-spec.js
@@ -8,4 +8,13 @@ describe("Match", function() {
       expect(theMatch.frames instanceof Array).toBe(true);
     });
   });
+
+  describe("addFrame", function() {
+    it("should accept frameId, frameStr as parameters to create object.", function() {
+      theMatch.addFrame(1, '-8');
+      expect(theMatch.frames.length).toBe(1);
+      theMatch.addFrame(2, '3/');
+      expect(theMatch.frames.length).toBe(2);
+    });
+  });
 });
